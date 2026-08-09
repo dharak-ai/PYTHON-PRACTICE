306b2088302e39c62127c023b5b5ad69a1ffa7a9
Dharak Patel <dharakpatel@Dharaks-MacBook-Pro.local>
2026-08-09 10:02:41 +0530

    Added marks checker

---
+++ marks checker.py
@@ -0,0 +1,11 @@
+# marks from highest to lowest
+
+marks = int(input("enter marks:"))
+if marks>=90:
+    print("A")
+elif marks>=80:
+    print("B")
+elif marks>=70:
+    print("C")
+else:
+    print("D")
